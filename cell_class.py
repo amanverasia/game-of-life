b7676dcacc76e5966fd6bcd093e4fc21b7a4edcf
Aman Verasia <verasiaaman3@gmail.com>
2021-01-04 22:12:16 +0530

    Update cell_class.py

--- cell_class.py
+++ cell_class.py
@@ -1,5 +1,5 @@
 import pygame
-
+import random
 
 class Cell:
 	def __init__(self, surface, grid_x, grid_y):
@@ -9,16 +9,42 @@ class Cell:
 		self.grid_y = grid_y
 		self.image = pygame.Surface((20,20))
 		self.rect = self.image.get_rect()
+		self.neighbours = []
+		self.alive_neighbours = 0
 
 	def update(self):
 		self.rect.topleft = (self.grid_x*20, self.grid_y*20)
-
-
+		for cell in self.neighbours:
+			if cell.alive:
+				cell.alive_neighbours += 1
 
 	def draw(self):
 		if self.alive:
 			self.image.fill((0,0,0))
 		else:
 			self.image.fill((0,0,0))
-			pygame.draw.rect(self.image, (255,255,255), (2,2,18,18))
+			pygame.draw.rect(self.image, (255,255,255), (1,1,18,18))
 		self.surface.blit(self.image, (self.grid_x*20, self.grid_y*20))
+
+
+	def get_neighbours(self, grid):
+		neighbour_list = [[1,-1], [1,0], [1,1], [-1,-1], [-1,0], [-1,1], [0,-1], [0,1]]
+		for neighbour in neighbour_list:
+			neighbour[0] += self.grid_x
+			neighbour[1] += self.grid_y
+		for neighbour in neighbour_list:
+			if neighbour[0] < 0:
+				neighbour[0]+=30
+			if neighbour[1] < 0:
+				neighbour[1]+=30
+			if neighbour[1] > 29:
+				neighbour[1]-=30
+			if neighbour[0] > 29:
+				neighbour[0]-=30
+			
+		for neighbour in neighbour_list:
+			try:
+				self.neighbours.append(grid[neighbour[1]][neighbour[0]])
+				#print(neighbour)
+			except:
+				print(neighbour)
